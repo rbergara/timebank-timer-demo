c27017b5b7ee74809229790ff02d59f1d1a34170
rbergara <rod.bch@gmail.com>
2022-12-15 11:41:13 -0600

    add timer info on html title

--- components/TimebankContainer.jsx
+++ components/TimebankContainer.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useRef, useState } from "react"
 import Counter from '../components/Counter';
 import Timebank from '../components/Timebank';
+import { displayTime } from "../lib/utils";
 
-const TimebankContainer = () => {
+const TimebankContainer = ({ setTitle }) => {
   const [current, setCurrent] = useState(0);
   const [accumulated, setAccumulated] = useState(0);
   const [running, setRunning] = useState(false);
@@ -13,11 +14,14 @@ const TimebankContainer = () => {
     if (running) {
       const started = new Date().getTime();
       const intrvlId = setInterval(function() {
-        setCurrent(new Date().getTime() - started);
+        const curr = new Date().getTime() - started
+        setCurrent(curr);
+        setTitle(`running - ${displayTime(curr)}`);
       }, 1000)
       intervalRef.current = intrvlId;
     } else {
       setAccumulated(a => a += current);
+      setTitle(`banked - ${displayTime(accumulated + current)}`);
       setCurrent(0);
       setRunning(false);
       clearInterval(intervalRef.current);
@@ -31,6 +35,7 @@ const TimebankContainer = () => {
       setAccumulated(0);
       setRunning(false);
       setReseted(false);
+      setTitle('Timebank Timer');
     }
   },[reseted]);
 
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,23 @@
 import Head from 'next/head'
+import { useEffect, useState } from 'react';
 import Header from '../components/Header'
 import TimebankContainer from '../components/TimebankContainer';
 
 const Home = () => {
+  const [title, setTitle] = useState('TimeBank Timer');
+
+  useEffect(() => {
+    document.title = title;
+  }, [title])
+
   return (
     <div className='min-h-screen flex flex-col items-center gap-y-16'>
       <Head>
-        <title>Timebank Timer</title>
+        <title className='text-red-500'>Timebank Timer</title>
         <link rel="icon" href="favicon.ico" />
       </Head>
       <Header />
-      <TimebankContainer />
+      <TimebankContainer setTitle={setTitle} />
     </div>
   )
 }
